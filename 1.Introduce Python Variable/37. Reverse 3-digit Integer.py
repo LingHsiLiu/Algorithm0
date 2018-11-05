79c612ed30dbd851613db7bc4d97f7c577892dfe
Althea <lhliu2@illinois.edu>
2018-11-04 19:50:41 -0600

    Update 37. Reverse 3-digit Integer.py

--- 1.Introduce Python Variable/37. Reverse 3-digit Integer.py
+++ 1.Introduce Python Variable/37. Reverse 3-digit Integer.py
@@ -1,1 +1,19 @@
+# 37. Reverse 3-digit Integer
+# Reverse a 3-digit integer.
 
+# Example
+# Reverse 123 you will get 321.
+
+# Reverse 900 you will get 9.
+
+class Solution:
+    """
+    @param number: A 3-digit number.
+    @return: Reversed number.
+    """
+    def reverseInteger(self, number):
+        # write your code here
+        num1 = number % 10
+        num2 = (number // 10 ) % 10
+        num3 = number // 100
+        return num1 * 100 + num2 * 10 + num3
