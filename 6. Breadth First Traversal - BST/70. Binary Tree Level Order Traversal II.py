4d05b7e180ec199b9e6aa40a6a7d9359f262243b
Althea <lhliu2@illinois.edu>
2018-11-05 00:02:50 -0600

    Create 70. Binary Tree Level Order Traversal II.py

---
+++ 6. Breadth First Traversal - BST/70. Binary Tree Level Order Traversal II.py
@@ -0,0 +1,35 @@
+# 70. Binary Tree Level Order Traversal II
+# Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+# Example
+# Given binary tree {3,9,20,#,#,15,7},
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: A tree
+    @return: buttom-up level order a list of lists of integer
+    """
+    def levelOrderBottom(self, root):
+        # write your code here
+        self.results = []
+        if not root:
+            return self.results
+        q = [root]
+        while q:
+            new_q = []
+            self.results.append([n.val for n in q])
+            for node in q:
+                if node.left:
+                    new_q.append(node.left)
+                if node.right:
+                    new_q.append(node.right)
+            q = new_q
+        return list(reversed(self.results))
