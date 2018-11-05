931339dad5580593c6647763b8205c7ac969dfff
Althea <lhliu2@illinois.edu>
2018-11-04 20:19:54 -0600

    Create 627. Longest Palindrome.py

---
+++ 1.Introduce Python Variable/627. Longest Palindrome.py
@@ -0,0 +1,29 @@
+# 627. Longest Palindrome
+# Given a string which consists of lowercase or uppercase letters, find the length of the longest palindromes that can be built with those letters.
+
+# This is case sensitive, for example "Aa" is not considered a palindrome here.
+
+# Example
+# Given s = "abccccdd" return 7
+
+# One longest palindrome that can be built is "dccaccd", whose length is 7.
+
+class Solution:
+    """
+    @param s: a string which consists of lowercase or uppercase letters
+    @return: the length of the longest palindromes that can be built
+    """
+    
+    def longestPalindrome(self, s):
+        # write your code here
+        dic = {}
+        for i in s:
+            if i in dic:
+                del dic[i]
+            else:
+                dic[i] = True
+        print(dic)
+        remove = len(dic)
+        if remove > 0:
+            remove = remove -1
+        return len(s) - remove
