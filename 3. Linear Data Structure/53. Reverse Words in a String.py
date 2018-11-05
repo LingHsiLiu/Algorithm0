96d2737057a60007c87bb0a2d4a7234e6c6d1515
Althea <lhliu2@illinois.edu>
2018-11-04 22:07:38 -0600

    Create 53. Reverse Words in a String.py

---
+++ 3. Linear Data Structure/53. Reverse Words in a String.py
@@ -0,0 +1,19 @@
+# 53. Reverse Words in a String
+# Given an input string, reverse the string word by word.
+
+# For example,
+# Given s = "the sky is blue",
+# return "blue is sky the".
+
+class Solution:
+    """
+    @param: s: A string
+    @return: A string
+    """
+    def reverseWords(self, s):
+        # write your code here
+        # print(s)
+        # s = s.split(" ")
+        print(s)
+        # return " ".join(s.split(" ")[::-1])
+        return ' '.join(reversed(s.strip().split())) ## miss space
