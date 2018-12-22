1455f964367b938aa6768b8ee5f505d5234d1104
Althea <lhliu2@illinois.edu>
2018-12-21 23:36:37 -0600

    Create 112. Remove Duplicates from Sorted List.py

---
+++ 3. Linear Data Structure/112. Remove Duplicates from Sorted List.py
@@ -0,0 +1,53 @@
+# 112. Remove Duplicates from Sorted List
+# Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+# Example
+# Given 1->1->2, return 1->2.
+# Given 1->1->2->3->3, return 1->2->3.
+
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param head: head is the head of the linked list
+    @return: head of linked list
+    """
+    def deleteDuplicates(self, head):
+        # write your code here
+
+
+public class Solution {
+    /**
+     * @param nums: a rotated sorted array
+     * @return: the minimum number in the array
+     */
+    public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        int start = 0, end = nums.length - 1;
+        int target = nums[nums.length - 1];
+        
+        // find the first element <= target
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= target) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        if (nums[start] <= target) {
+            return nums[start];
+        } else {
+            return nums[end];
+        }
+    }
+}
