925f045ec67fee1fcdce28f977ac20fd64fe7ff2
Althea <lhliu2@illinois.edu>
2019-01-02 12:52:40 -0600

    Create 644. Strobogrammatic Number.py

---
+++ 7. Set- Dictionary- Divide - Conquer/644. Strobogrammatic Number.py
@@ -0,0 +1,19 @@
+# 644. Strobogrammatic Number
+# A mirror number is a number that looks the same when rotated 180 degrees (looked at upside down).
+
+# Write a function to determine if a number is mirror. The number is represented as a string.
+
+# Example
+# For example, the numbers "69", "88", and "818" are all mirror numbers.
+# Given num = "69" return true
+# Given num = "68" return false
+
+
+
+class Solution:
+    """
+    @param num: a string
+    @return: true if a number is strobogrammatic or false
+    """
+    def isStrobogrammatic(self, num):
+        # write your code here
