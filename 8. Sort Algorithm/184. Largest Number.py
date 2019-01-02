4a0d452856d6b3373b45c331069f72c2653bd8f7
Althea <lhliu2@illinois.edu>
2019-01-02 13:02:23 -0600

    Create 184. Largest Number.py

---
+++ 8. Sort Algorithm/184. Largest Number.py
@@ -0,0 +1,16 @@
+# 184. Largest Number
+# Given a list of non negative integers, arrange them such that they form the largest number.
+
+# Example
+# Given [1, 20, 23, 4, 8], the largest formed number is 8423201.
+
+# Challenge
+# Do it in O(nlogn) time complexity.
+
+class Solution:
+    """
+    @param nums: A list of non negative integers
+    @return: A string
+    """
+    def largestNumber(self, nums):
+        # write your code here
