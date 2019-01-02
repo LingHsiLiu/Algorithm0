4f23d9de4dd3f9d9306b2fade5f0d7bf9a18a462
Althea <lhliu2@illinois.edu>
2019-01-02 11:51:31 -0600

    Create 146. Lowercase to Uppercase II.py

---
+++ 3. Linear Data Structure/146. Lowercase to Uppercase II.py
@@ -0,0 +1,19 @@
+# 146. Lowercase to Uppercase II
+# Implement an upper method to convert all characters in a string to uppercase.
+
+# You should ignore the characters not in alphabet.
+
+# Example
+# Given "abc", return "ABC".
+
+# Given "aBc", return "ABC".
+
+# Given "abC12", return "ABC12".
+
+class Solution:
+    """
+    @param str: A string
+    @return: A string
+    """
+    def lowercaseToUppercase2(self, str):
+        # write your code here
