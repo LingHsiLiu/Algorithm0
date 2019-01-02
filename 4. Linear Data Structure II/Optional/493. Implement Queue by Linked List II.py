d94fc7b0ec1d8081470986305cbe64b2599bfc56
Althea <lhliu2@illinois.edu>
2019-01-02 12:08:06 -0600

    Create 493. Implement Queue by Linked List II.py

---
+++ 4. Linear Data Structure II/Optional/493. Implement Queue by Linked List II.py
@@ -0,0 +1,47 @@
+# 493. Implement Queue by Linked List II
+# Implement a Queue by linked list. Provide the following basic methods:
+
+# push_front(item). Add a new item to the front of queue.
+# push_back(item). Add a new item to the back of the queue.
+# pop_front(). Move the first item out of the queue, return it.
+# pop_back(). Move the last item out of the queue, return it.
+# Example
+# push_front(1)
+# push_back(2)
+# pop_back() // return 2
+# pop_back() // return 1
+# push_back(3)
+# push_back(4)
+# pop_front() // return 3
+# pop_front() // return 4
+
+class Dequeue:
+    
+    def __init__(self):
+        # do intialization if necessary
+
+    """
+    @param: item: An integer
+    @return: nothing
+    """
+    def push_front(self, item):
+        # write your code here
+
+    """
+    @param: item: An integer
+    @return: nothing
+    """
+    def push_back(self, item):
+        # write your code here
+
+    """
+    @return: An integer
+    """
+    def pop_front(self):
+        # write your code here
+
+    """
+    @return: An integer
+    """
+    def pop_back(self):
+        # write your code here
