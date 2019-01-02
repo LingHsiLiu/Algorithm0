c4d5ee3af66a8c42fe6de74a115aea8b818cb067
Althea <lhliu2@illinois.edu>
2019-01-02 13:00:07 -0600

    Create 173. Insertion Sort List.py

---
+++ 8. Sort Algorithm/173. Insertion Sort List.py
@@ -0,0 +1,22 @@
+# 173. Insertion Sort List
+# Sort a linked list using insertion sort.
+
+# Example
+# Given 1->3->2->0->null, return 0->1->2->3->null.
+
+
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param head: The first node of linked list.
+    @return: The head of linked list.
+    """
+    def insertionSortList(self, head):
+        # write your code here
