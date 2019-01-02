2d3dd980eb828c1e36c802cd1d7707c511015fe1
Althea <lhliu2@illinois.edu>
2019-01-02 12:11:29 -0600

    Create 451. Swap Nodes in Pairs.py

---
+++ 4. Linear Data Structure II/Optional/451. Swap Nodes in Pairs.py
@@ -0,0 +1,21 @@
+# 451. Swap Nodes in Pairs
+# Given a linked list, swap every two adjacent nodes and return its head.
+
+# Example
+# Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param head: a ListNode
+    @return: a ListNode
+    """
+    def swapPairs(self, head):
+        # write your code here
