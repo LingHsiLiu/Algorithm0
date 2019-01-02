e9a165147eca51c351d19d99a3d9657708fb637c
Althea <lhliu2@illinois.edu>
2019-01-02 12:14:24 -0600

    Create 511. Swap Two Nodes in Linked List.py

---
+++ 4. Linear Data Structure II/Optional/511. Swap Two Nodes in Linked List.py
@@ -0,0 +1,25 @@
+# 511. Swap Two Nodes in Linked List
+# Given a linked list and two values v1 and v2. Swap the two nodes in the linked list with values v1 and v2. It's guaranteed there is no duplicate values in the linked list. If v1 or v2 does not exist in the given linked list, do nothing.
+
+# Example
+# Given 1->2->3->4->null and v1 = 2, v2 = 4.
+
+# Return 1->4->3->2->null.
+
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param head: a ListNode
+    @param v1: An integer
+    @param v2: An integer
+    @return: a new head of singly-linked list
+    """
+    def swapNodes(self, head, v1, v2):
+        # write your code here
