9ce81ffe27c35c08b898646b1c6e75e19bc78f67
Althea <lhliu2@illinois.edu>
2019-01-02 11:58:15 -0600

    Create 483. Convert Linked List to Array List.py

---
+++ 4. Linear Data Structure II/483. Convert Linked List to Array List.py
@@ -0,0 +1,21 @@
+# 483. Convert Linked List to Array List
+# Convert a linked list to an array list.
+
+# Example
+# Given 1->2->3->null, return [1,2,3].
+
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param head: the head of linked list.
+    @return: An integer list
+    """
+    def toArrayList(self, head):
+        # write your code here
