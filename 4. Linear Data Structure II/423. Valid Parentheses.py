93fbc3a00e4c05cd2916f4ac7d33735d3ad66b37
Althea <lhliu2@illinois.edu>
2019-01-02 12:03:28 -0600

    Create 423. Valid Parentheses.py

---
+++ 4. Linear Data Structure II/423. Valid Parentheses.py
@@ -0,0 +1,13 @@
+# 423. Valid Parentheses
+# Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# Example
+# The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+class Solution:
+    """
+    @param s: A string
+    @return: whether the string is a valid parentheses
+    """
+    def isValidParentheses(self, s):
+        # write your code here
