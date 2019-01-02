68bbb12b15d639a2b08280ce533afb2d01441f3e
Althea <lhliu2@illinois.edu>
2019-01-02 12:56:40 -0600

    Create 557. Count Characters.py

---
+++ 7. Set- Dictionary- Divide - Conquer/Optional/557. Count Characters.py
@@ -0,0 +1,20 @@
+# 557. Count Characters
+# Count characters in a string. Return a hash map which key is character and value is the occurrency of this character.
+
+# Example
+# Given str = "abca", return
+
+# {
+#   "a": 2,
+#   "b": 1,
+#   "c": 1
+# }
+
+class Solution:
+    """
+    @param: : a string
+    @return: Return a hash map
+    """
+
+    def countCharacters(self, str):
+        # write your code here
