43dc6c9fd5802c0720cdc82bfe76d5a653913fec
Althea <lhliu2@illinois.edu>
2019-01-02 12:07:16 -0600

    Create 228. Middle of Linked List.py

---
+++ 4. Linear Data Structure II/Optional/228. Middle of Linked List.py
@@ -0,0 +1,23 @@
+# 228. Middle of Linked List
+# Find the middle node of a linked list.
+
+# Example
+# Given 1->2->3, return the node with value 2.
+
+# Given 1->2, return the node with value 1.
+
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param head: the head of linked list.
+    @return: a middle node of the linked list
+    """
+    def middleNode(self, head):
+        # write your code here
