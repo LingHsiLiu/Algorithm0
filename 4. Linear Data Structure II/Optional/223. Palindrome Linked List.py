401c4db69ec1cb6f93cfee6c202ae4c963648cd0
Althea <lhliu2@illinois.edu>
2019-01-02 12:15:59 -0600

    Create 223. Palindrome Linked List.py

---
+++ 4. Linear Data Structure II/Optional/223. Palindrome Linked List.py
@@ -0,0 +1,24 @@
+# 223. Palindrome Linked List
+# Implement a function to check if a linked list is a palindrome.
+
+# Example
+# Given 1->2->1, return true
+
+# Challenge
+# Could you do it in O(n) time and O(1) space?
+
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param head: A ListNode.
+    @return: A boolean.
+    """
+    def isPalindrome(self, head):
+        # write your code here
